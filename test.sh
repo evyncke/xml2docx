81f5135af9157067b0b6de8f7e9ac610f87ff1ac
Eric Vyncke <eric@vyncke.org>
2020-09-08 17:44:27 +0200

    Remove hidden MacOS file from the final DOCX

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
-./xml2docx.py $1
-mv xml2doc.xml template/word/document.xml 
+./xml2docx.py -i $1
+mv xml2docx.xml template/word/document.xml 
 cd template/
 zip -r ../test.docx *
 cd -
+zip --delete test.docx word/.DS_Store 
